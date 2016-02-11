2122972c66211fc979b6936411982eae973cbbd7
Ustinov Valeryi <ustinov.vd@gmail.com>
2016-02-11 14:42:02 +0600

    /word <number>, fix in data

--- havamal.py
+++ havamal.py
@@ -17,18 +17,34 @@ def handle(msg):
     command = msg['text']
 
     print 'Got command: %s' % command
-
-    if command == '/word':
-        bot.sendMessage(chat_id, 'Слово Высокого')
-    elif command == '/words':
+    conn = connect_db()
+    cursor = conn.cursor()
+    if command == '/words':
         word_id = random.randint(1,164)
-        params =(word_id,)
-        conn = connect_db()
-        cursor = conn.cursor()
+        params = (word_id,)
         cursor.execute('SELECT * FROM words where id=?', params)
         fetch = cursor.fetchone()
         result = fetch[1].encode('utf-8').strip()
         bot.sendMessage(chat_id, result)
+
+    command_list = command.split(" ")
+    if command_list[0] == '/word':
+        #get first argument from command, it should be a number between 1 and 164
+        word_id = command_list[1]
+        try:
+            word_id = int(word_id)
+        except ValueError:
+            bot.sendMessage(chat_id, 'Один такого не говорил!!!')
+        else:
+            if word_id > 164 or word_id < 1:
+                bot.sendMessage(chat_id, 'Один такого не говорил!')
+            else:
+                params = (word_id,)
+                cursor.execute('SELECT * FROM words where id=?', params)
+                fetch = cursor.fetchone()
+
+                result = fetch[1].encode('utf-8').strip()
+                bot.sendMessage(chat_id, result)
 
 bot = telepot.Bot(TOKEN)
 bot.notifyOnMessage(handle)
